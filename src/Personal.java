1fb34a39897090d4d6f5c31b3246fcd954c68983
bhumika <bhumikabhadani69@gmail.com>
2023-03-12 17:51:48 +0000

    added first java

---
+++ src/Personal.java
@@ -0,0 +1,23 @@
+public class Personal {
+    //global static variable
+    static String name = "Ram";
+    static String address = "15,kenton road";
+    static long phonenumber = 9924343001L;
+    static int age = 20;
+    static int rollnomber = 4;
+    static char grade = 'A';
+    static boolean eligibility = true;
+    static double amount  = 20.10;
+    // main method
+    public static void main(String[] args){
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(phonenumber);
+        System.out.println(age);
+        System.out.println(rollnomber);
+        System.out.println(grade);
+        System.out.println(eligibility);
+        System.out.println(amount);
+    }
+
+}
